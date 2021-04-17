0a8ac8e5d6e24b4102c8f98ddc6e22f809899aa3
NKWETACHA TERENCE NGWEN <78331614+ROOT-017@users.noreply.github.com>
2021-04-17 03:02:23 +0100

    Factorial by ROOT-017

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+//Predefint factorial for Number(num) == 5
+//DECLERE VARIABLE NOT WARS!! copyright©2021
+//By ROOT😉
+public class Factorial {
+    public static int num = 5, i, fact = 1;
+
+    public static void main(String[] args) {
+        for (i = 1; i <= num; i++) {
+            fact = fact * i;
+
+        }
+        System.out.println("Fact:" + fact);
+    }
+
+}
