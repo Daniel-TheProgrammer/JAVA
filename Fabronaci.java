bf375b8e47213914bb3b9fc06b394d5729912330
NKWETACHA TERENCE NGWEN <78331614+ROOT-017@users.noreply.github.com>
2021-04-17 03:41:47 +0100

    Fibronaci By ROOT-017

---
+++ Fabronaci.java
@@ -0,0 +1,16 @@
+//Predefint factorial for Number(num) == 5
+//DECLERE VARIABLE NOT WARS!! copyright©2021
+//By ROOT😉
+public class Fabronaci {
+
+    public static int num = 5, fad = 1, i;
+
+    public static void main(String[] args) {
+        for (i = 0; i <= num; i++) {
+            fad += i - 1;
+
+        }
+        System.out.println("Fabronaci of " + num + ": " + fad);
+    }
+
+}
