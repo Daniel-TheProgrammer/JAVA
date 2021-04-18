83bc5e67eba65abed2679fa13340ddc5a8bc21a5
NKWETACHA TERENCE NGWEN <78331614+ROOT-017@users.noreply.github.com>
2021-04-18 22:40:41 +0100

    Factorial and Fibronaci

--- Factorial.java
+++ Factorial.java
@@ -1,8 +1,8 @@
-//Predefint factorial for Number(num) == 5
+//Predefint factorial and Fabronci for Number(num or n) == 5
 //DECLERE VARIABLE NOT WARS!! copyright©2021
 //By ROOT😉
 public class Factorial {
-    public static int num = 5, i, fact = 1;
+    public static int num = 5, i, fact = 1, n = 5, fad = 1, j;;
 
     public static void main(String[] args) {
         for (i = 1; i <= num; i++) {
@@ -10,23 +10,11 @@ public class Factorial {
 
         }
         System.out.println("Fact:" + fact);
-    }
-
-}
-
-// Predefint Fabronaci for Number(num) == 5
-// DECLERE VARIABLE NOT WARS!! copyright©2021
-// By ROOT...😉
-public class Fabronaci {
-
-    public static int num = 5, fad = 1, i;
-
-    public static void main(String[] args) {
-        for (i = 0; i <= num; i++) {
-            fad += i - 1;
+        for (j = 0; j <= n; j++) {
+            fad += j - 1;
 
         }
-        System.out.println("Fabronaci of " + num + ": " + fad);
+        System.out.println("Fabronaci of " + n + ": " + fad);
     }
 
 }
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,16 +1,20 @@
-// Predefint Fabronaci for Number(num) == 5
-// DECLERE VARIABLE NOT WARS!! copyright©2021
-// By ROOT😉
-public class Fabronaci {
-
-    public static int num = 5, fad = 1, i;
+//Predefint factorial for Number(num) == 5
+//DECLERE VARIABLE NOT WARS!! copyright©2021
+//By ROOT😉
+public class Factorial {
+    public static int num = 5, i, fact = 1, n = 5, fad = 1, j;;
 
     public static void main(String[] args) {
-        for (i = 0; i <= num; i++) {
-            fad += i - 1;
+        for (i = 1; i <= num; i++) {
+            fact = fact * i;
 
         }
-        System.out.println("Fabronaci of " + num + ": " + fad);
+        System.out.println("Fact:" + fact);
+        for (j = 0; j <= n; j++) {
+            fad += j - 1;
+
+        }
+        System.out.println("Fabronaci of " + n + ": " + fad);
     }
 
 }
