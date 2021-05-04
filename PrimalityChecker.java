f62d16c3a87a5000706c6fec8df91d08172cdb73
NKWETACHA TERENCE NGWEN <78331614+ROOT-017@users.noreply.github.com>
2021-05-04 12:41:14 +0100

    Great Changes Performed

--- PrimalityChecker.java
+++ PrimalityChecker.java
@@ -11,28 +11,27 @@ public class PrimalityChecker {
 
     public static int isPrime(int n) {
 
-        if (n <= 1){
+        if (n <= 1) {
             System.out.println("Number Is Non Prime");
         }
 
-        else if(n>=2){
-            for (int i = 2; i < n; i++){
+        else if (n >= 2) {
+            for (int i = 2; i < n; i++) {
                 if (n % i == 0)
-                System.out.println("Number Is Non Prime")
-        }
-        }
-        else{
-            System.out.println("Number Is Non Prime")
+                    System.out.println("Number Is Non Prime");
+            }
+        } else {
+            System.out.println("Number Is Non Prime");
         }
     }
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
         scan = new Scanner(System.in);
 
         System.out.print("Enter a number:");
         num = scan.nextInt();
 
-        if (isPrime(num)) {
+        if (isPrime(num)) /*{
             System.out.println("\nTrue");
         } else {
             System.out.println("\nFalse");
@@ -43,4 +42,6 @@ public class PrimalityChecker {
             System.out.println("\nFalse");
         }
     }
+    */
 }
+};
