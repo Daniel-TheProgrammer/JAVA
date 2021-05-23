53cf7a6e967700523141fe322800019e26356581
NKWETACHA TERENCE NGWEN <78331614+ROOT-017@users.noreply.github.com>
2021-05-23 02:47:25 +0100

    Program to printe current date and time

---
+++ CreatesDate.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+class CreatesDate {
+    public static void main(String args[]) {
+        Date d1;
+
+        d1 = new Date();
+        System.out.println("Date 1: " + d1);
+    }
+
+}
