2eefcbdaee6ee696ad748384fedca650fc1d4fcc
NKWETACHA TERENCE NGWEN <78331614+ROOT-017@users.noreply.github.com>
2021-05-23 04:26:20 +0100

    No  big changes

--- CreatesDate.java
+++ CreatesDate.java
@@ -4,6 +4,7 @@ public class CreatesDate {
 
 
     public static void main(String args[]) {
+        int  a,b ,c;
         Date d1;
         d1 = new Date();
         System.out.println("Date 1: " + d1);
