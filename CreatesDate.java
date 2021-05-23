359ab51341643043960073eec4a6810e5fe8230f
NKWETACHA TERENCE NGWEN <78331614+ROOT-017@users.noreply.github.com>
2021-05-23 02:58:02 +0100

    Minimal changes

--- CreatesDate.java
+++ CreatesDate.java
@@ -1,11 +1,14 @@
 import java.util.Date;
 
-class CreatesDate {
+public class CreatesDate {
+
+
     public static void main(String args[]) {
         Date d1;
-
         d1 = new Date();
         System.out.println("Date 1: " + d1);
+        
+
     }
 
 }
