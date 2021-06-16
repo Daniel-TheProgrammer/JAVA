5d62e8cd7e3bf969d9a9bfcf04ee1207514a7a7e
NKWETACHA TERENCE NGWEN <78331614+ROOT-017@users.noreply.github.com>
2021-06-17 00:50:32 +0100

    hxx

---
+++ Java Basic codes/small.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, sum;
+    printf("Enter a numer: ");
+    scanf("%d", &a);
+    printf("Enter a Second number: ");
+    scanf("%d", &b);
+
+    sum = a + b;
+
+    printf("Sum = %d ", sum);
+    return 0;
+}
